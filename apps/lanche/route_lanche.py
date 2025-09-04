08a48ac7e3eca19b33c26c0a1430b056d5fa1f71
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-04 08:50:37 -0300

    Update: erro na importação do Blueprint

--- apps/lanche/route_lanche.py
+++ apps/lanche/route_lanche.py
@@ -1,7 +1,7 @@
-from flask import blueprints, request, jsonify
+from flask import Blueprint, request, jsonify
 import lanche.model_lanche as modLan
 
-bd_Lanche = blueprints('Lanche', __name__)
+bd_Lanche = Blueprint('Lanche', __name__)
 
 @bd_Lanche.route("/lanche", methods=["GET"])
 def listar_lanche():
