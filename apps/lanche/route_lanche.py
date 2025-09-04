98231eaca2dfd60e1eb34d777e9d0d70a5e33f66
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-04 10:11:23 -0300

    feat: definindo rota criar_lanche

--- apps/lanche/route_lanche.py
+++ apps/lanche/route_lanche.py
@@ -10,3 +10,24 @@ def listar_lanche():
         return jsonify(lanche)
     except Exception as e:
         return {"Erro": str(e)},400
+    
+@bd_Lanche.route("/lanche",methods=["POST"])
+def criar_lanche():
+    nv_dict = request.json
+    nv_dict["id"] = int(nv_dict["id"])
+    nv_dict["preco"] = float(nv_dict["preco"])
+
+    try:
+        if modLan.lancheExiste(nv_dict["id"]):
+            return jsonify({
+                "Erro": "Requisição inválida",
+                "Detalhes": str(modLan.LancheJaExiste)
+            }), 409
+        
+        modLan.criarLanche(nv_dict)
+
+    except modLan.CadastroDeLancheFalhado as cdt:
+        return jsonify({
+            "Erro": "Falha ao cadastrar lancher",
+            "Detalhes": str(cdt)
+        }), 400
