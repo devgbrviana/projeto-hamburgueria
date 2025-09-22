3ffe85384e0810e2af4033bf427e6c8d7c398ae5
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-22 16:29:12 -0300

    Feat(backend/usuario): Adicionando esqueleto das rotas CRUD para cadastro de usuário

---
+++ apps/usuario/route_usuario.py
@@ -0,0 +1,28 @@
+from flask import request, jsonify, Blueprint
+import usuario.model_usuario as ModUso
+
+bd_Usuario = Blueprint("Usuario", __name__)
+
+@bd_Usuario.route("/usuario", methods=["GET"])
+def listar_usuario():
+    pass
+
+
+@bd_Usuario.route("/usuario/<int:id>", methods=["GET"])
+def listar_usuario_id(id):
+    pass
+
+
+@bd_Usuario.route("/usuario", methods=["POST"])
+def criar_usuario():
+    pass
+
+
+@bd_Usuario.route("/usuario/<int:id>", methods=["DELETE"])
+def deletar_usuario(id):
+    pass
+
+
+bd_Usuario.route("/usuario/<int:id>", methods=["PUT"])
+def alterar_usuario(id):
+    pass
