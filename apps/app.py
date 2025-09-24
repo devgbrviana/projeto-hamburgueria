93fad3abaa43499ce697c1f76fc93338ab58f9fb
Eduardo N. <eduardonunesdasilva23@gmail.com>
2025-09-24 19:08:01 -0300

    Feat: registrando a rota no app.py

--- apps/app.py
+++ apps/app.py
@@ -1,6 +1,7 @@
 from config import app, db_serv
 from lanche.route_lanche import bd_Lanche
 from usuario.route_usuario import bd_Usuario
+from login.route_login import bd_Login
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -8,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app.register_blueprint(bd_Lanche)
 app.register_blueprint(bd_Usuario)
+app.register_blueprint(bd_Login)
 
 app.config['JSON_AS_ASCII'] = False
 if __name__ == '__main__':
