e9d1fe2fdf69844b57e62ac2fcfd5b537561b6f9
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-11-29 17:19:34 -0300

    feat: Categorias: pizzas,vegetariano e kids adicionados no arquivo init_db

--- apps/init_db.py
+++ apps/init_db.py
@@ -6,28 +6,56 @@ def seed_database():
     """Popula a tabela de lanches com dados de exemplo."""
     lanches_de_exemplo = [
         {
-            "id": 1, "nome": "Java Burguer", "preco": 31.99,
-            "descricao": "Pão com gergelim, um suculento hambúrguer de pura carne bovina, cheddar fatiado, molho barbecue, 6 deliciosas onion rings, tomate, alface e maionese."
+            "id": 1, "nome": "Java Burguer", "preco": 31.99,"categoria": "Burgers",
+            "descricao": """Pão com gergelim, um suculento hambúrguer de pura carne bovina, cheddar fatiado, molho barbecue,
+            6 deliciosas onion rings, tomate, alface e maionese."""
         },
         {
-            "id": 2, "nome": "Kotlin Burguer", "preco": 36.5,
-            "descricao": "Pão com gergelim, dois suculentos hambúrgueres de pura carne bovina, duas fatias de cheddar, quatro fatias de picles, alface, tomate, cebola, maionese e ketchup."
+            "id": 2, "nome": "Kotlin Burguer", "preco": 36.5,"categoria": "Burgers",
+            "descricao": """Pão com gergelim, dois suculentos hambúrgueres de pura carne bovina, duas fatias de cheddar, 
+            quatro fatias de picles, alface, tomate, cebola, maionese e ketchup."""
         },
         {
-            "id": 3, "nome": "Python Burguer", "preco": 33.99,
-            "descricao": "Pão com gergelim, um saboroso hambúrguer de pura carne bovina, uma fatia de queijo cheddar, duas fatias de picles, ketchup e molho mostarda."
+            "id": 3, "nome": "Python Burguer", "preco": 33.99,"categoria": "Burgers",
+            "descricao": """Pão com gergelim, um saboroso hambúrguer de pura carne bovina,
+            uma fatia de queijo cheddar, duas fatias de picles, ketchup e molho mostarda."""
         },
         {
-            "id": 4, "nome": "SQL Burguer", "preco": 39.99,
-            "descricao": "Um hambúrguer (carne 100% bovina), bacon, alface americana, cebola, queijo processado sabor cheddar, tomate, maionese, ketchup, mostarda e pão com gergelim."
+            "id": 4, "nome": "SQL Burguer", "preco": 39.99,"categoria": "Burgers",
+            "descricao": """Um hambúrguer (carne 100% bovina), bacon, alface americana, cebola, queijo processado sabor cheddar, tomate, 
+            maionese, ketchup, mostarda e pão com gergelim."""
         },
         {
-            "id": 5, "nome": "PHP Burguer", "preco": 30.99,
-            "descricao": "Dois hambúrgueres (carne 100% bovina), queijo processado sabor cheddar, cebola, fatias de bacon, ketchup, mostarda e pão com gergelim."
+            "id": 5, "nome": "PHP Burguer", "preco": 30.99,"categoria": "Burgers",
+            "descricao": """Dois hambúrgueres (carne 100% bovina), queijo processado sabor cheddar, 
+            cebola, fatias de bacon, ketchup, mostarda e pão com gergelim."""
         },
         {
-            "id": 6, "nome": "JS Burguer", "preco": 36.99,
-            "descricao": "Os novos sanduíches contêm dois hambúrgueres de carne 100% bovina, com um peso total de 227,6g. Além da carne, a receita inclui a exclusiva maionese com sabor de carne defumada, fatias de bacon, queijo processado, molho especial e cebola ao molho shoyu."
+            "id": 6, "nome": "JS Burguer", "preco": 36.99,"categoria": "Burgers",
+            "descricao": """Os novos sanduíches contêm dois hambúrgueres de carne 100% bovina, com um peso total de 227,6g. 
+            Além da carne, a receita inclui a exclusiva maionese com sabor de carne defumada, fatias de bacon, queijo processado,
+            molho especial e cebola ao molho shoyu."""
+        },
+        # --- PIZZAS ---
+        {
+            "id": 10, "nome": "Pizza Pythonresa", "preco": 45.90, "categoria": "Pizza",
+            "descricao": "Molho de tomate, mussarela, calabresa e cebola."
+        },
+        {
+            "id": 11, "nome": "Pizza Marguerita", "preco": 42.00, "categoria": "Pizza",
+            "descricao": "Molho de tomate, mussarela, manjericão fresco e tomate."
+        },
+
+        # --- VEGETARIANO ---
+        {
+            "id": 20, "nome": "Veggie Node", "preco": 29.90, "categoria": "Vegetariano",
+            "descricao": "Hambúrguer de grão de bico, alface, tomate e molho de ervas."
+        },
+
+        # --- KIDS ---
+        {
+            "id": 30, "nome": "Júnior Script", "preco": 19.90, "categoria": "Kids",
+            "descricao": "Cheeseburger pequeno, batata frita e suco de laranja."
         }
     ]
 
