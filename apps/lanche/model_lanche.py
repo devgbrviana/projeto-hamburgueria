7ac395c70212885f9a47d95c6e96ecdd4cc2a3e5
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-11-29 16:51:36 -0300

    feat: Categoria adicionada na tabela lanches

--- apps/lanche/model_lanche.py
+++ apps/lanche/model_lanche.py
@@ -5,23 +5,25 @@ class Lanche (db_serv.Model):
     __table_args__ = {'extend_existing': True}
 
     id = db_serv.Column(db_serv.Integer, primary_key=True)
-    nome = db_serv.Column(db_serv.String(60), nullable=True)
+    nome = db_serv.Column(db_serv.String(60), nullable=False)
     preco = db_serv.Column(db_serv.Float, nullable=False)
     descricao = db_serv.Column(db_serv.String(450), nullable=False)
-
-
-    def __init__(self, id, nome, preco, descricao):
+    categoria = db_serv.Column(db_serv.String(50), nullable=False, default='Burgers')
+    
+    def __init__(self, id, nome, preco, descricao,categoria):
         self.id = id
         self.nome = nome
         self.preco = preco
         self.descricao = descricao
+        self.categoria = categoria
 
     def to_dict(self):
         return {
             "id": self.id,
             "nome": self.nome,
             "preco": self.preco,
-            "descricao": self.descricao
+            "descricao": self.descricao,
+            "categoria": self.categoria
         }
         
 ### ===== Classe de exceção ===== ###
